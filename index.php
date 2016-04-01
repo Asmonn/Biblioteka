40384c5fb406cab972e316a2054258eff9c3fa7e
Asmon <Asmon@Asmon-Komputer>
2016-04-01 10:16:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'config.php';
+        db_connect();
+        ?>
+        <p>Udało się połączyć do bazy danych</p>
+    </body>
+</html>
